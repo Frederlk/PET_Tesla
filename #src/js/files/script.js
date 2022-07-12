3d83bf1d3f3f9326dc811bf0b93151e9ffb085e2
Sergey Petrinich <petrinichshadow@gmail.com>
2022-07-12 11:37:43 +0300

    pet project dove v1 - 13.05.2021

---
+++ #src/js/files/script.js
@@ -0,0 +1,17 @@
+
+window.onload = function () {
+	const acceleration = {
+		decimal: ',',
+		decimalPlaces: 1,
+	};
+	const batteryOptions = {
+		separator: '',
+	};
+	let countUp = new CountUp('acceleration', 2.1, acceleration);
+	let battery = new CountUp('battery', 1000, batteryOptions);
+	let speed = new CountUp('speed', 400,);
+	countUp.start();
+	battery.start();
+	speed.start();
+}
+
