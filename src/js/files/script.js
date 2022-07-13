9fbdcf18d4938a0777747c5ac68cdefb6fc18a2b
Sergey Petrinich <petrinichshadow@gmail.com>
2022-07-13 17:53:11 +0300

    home-slider v2 \ uploaded video \ fixed js

--- src/js/files/script.js
+++ src/js/files/script.js
@@ -1,19 +1,48 @@
 // Подключение функционала "Чертогов Фрилансера"
-import { isMobile } from "./functions.js";
+import { CountUp } from "countup.js";
 // Подключение списка активных модулей
 
-// window.onload = function () {
-//     const acceleration = {
-//         decimal: ",",
-//         decimalPlaces: 1,
-//     };
-//     const batteryOptions = {
-//         separator: "",
-//     };
-//     let countUp = new CountUp("acceleration", 2.1, acceleration);
-//     let battery = new CountUp("battery", 1000, batteryOptions);
-//     let speed = new CountUp("speed", 400);
-//     countUp.start();
-//     battery.start();
-//     speed.start();
-// };
+if (document.querySelector(".info__video")) {
+    const videoFile = document.getElementById("video-file");
+    const videoWrap = document.getElementById("video-wrap");
+    let observer = new MutationObserver(function (mutations) {
+        for (let mutation of mutations) {
+            if (mutation.type === "attributes") {
+                if (videoWrap.classList.contains("_watcher-view") && videoFile.paused) {
+                    videoFile.play();
+                } else {
+                    videoFile.pause();
+                }
+            }
+        }
+    });
+
+    observer.observe(videoWrap, { attributes: true });
+}
+
+if (document.getElementById("count-up")) {
+    const countUpWrap = document.getElementById("count-up");
+    let observer = new MutationObserver(function (mutations) {
+        for (let mutation of mutations) {
+            if (mutation.type === "attributes") {
+                if (countUpWrap.classList.contains("_watcher-view")) {
+                    const acceleration = {
+                        decimal: ",",
+                        decimalPlaces: 1,
+                    };
+                    const batteryOptions = {
+                        separator: "",
+                    };
+                    let countUp = new CountUp("acceleration", 2.1, acceleration);
+                    let battery = new CountUp("battery", 1000, batteryOptions);
+                    let speed = new CountUp("speed", 400);
+                    countUp.start();
+                    battery.start();
+                    speed.start();
+                }
+            }
+        }
+    });
+
+    observer.observe(countUpWrap, { attributes: true });
+}
