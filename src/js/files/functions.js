f85c7fccae9238f51f795aeac026b2a86af30769
Sergey Petrinich <petrinichshadow@gmail.com>
2022-07-14 17:59:35 +0300

    scss v2 \ html v2 \ need to fix videos

--- src/js/files/functions.js
+++ src/js/files/functions.js
@@ -64,16 +64,23 @@ export function fullVHfix() {
 }
 // Вспомогательные модули блокировки прокрутки и скочка ====================================================================================================================================================================================================================================================================================
 export let bodyLockStatus = true;
-export let bodyLockToggle = (delay = 300) => {
+export let bodyLockToggle = (delay = 500) => {
     if (document.documentElement.classList.contains("lock")) {
         bodyUnlock(delay);
     } else {
         bodyLock(delay);
     }
 };
-export let bodyUnlock = (delay = 300) => {
+export let bodyUnlock = (delay = 500) => {
+    let body = document.querySelector("body");
     if (bodyLockStatus) {
+        let lock_padding = document.querySelectorAll("[data-lp]");
         setTimeout(() => {
+            for (let index = 0; index < lock_padding.length; index++) {
+                const el = lock_padding[index];
+                el.style.paddingRight = "0px";
+            }
+            body.style.paddingRight = "0px";
             document.documentElement.classList.remove("lock");
         }, delay);
         bodyLockStatus = false;
@@ -82,9 +89,17 @@ export let bodyUnlock = (delay = 300) => {
         }, delay);
     }
 };
-export let bodyLock = (delay = 300) => {
+export let bodyLock = (delay = 500) => {
+    let body = document.querySelector("body");
     if (bodyLockStatus) {
+        let lock_padding = document.querySelectorAll("[data-lp]");
+        for (let index = 0; index < lock_padding.length; index++) {
+            const el = lock_padding[index];
+            el.style.paddingRight = window.innerWidth - document.querySelector(".wrapper").offsetWidth + "px";
+        }
+        body.style.paddingRight = window.innerWidth - document.querySelector(".wrapper").offsetWidth + "px";
         document.documentElement.classList.add("lock");
+
         bodyLockStatus = false;
         setTimeout(function () {
             bodyLockStatus = true;
